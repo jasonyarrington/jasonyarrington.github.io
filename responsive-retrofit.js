f9b1cf7e44a8cf39b34de0b72f7ca53876e9a9b0
Jason Yarrington <jason@ffinno.com>
2014-06-08 18:32:14 -0400

    Hosted script

---
+++ responsive-retrofit.js
@@ -0,0 +1,15 @@
+(function() {
+
+	var cssLink = document.createElement('link');
+	cssLink.setAttribute('rel', 'stylesheet');
+	cssLink.setAttribute('type', 'text/stylesheet');
+	cssLink.setAttribute('href', "https://github.com/jasonyarrington/Responsive-Retrofitting/" + document.location.hostname + "/css/retrofit.css");
+	document.head.appendChild(cssLink);
+
+	// Add meta tag, too!
+	var metaCode = document.createElement('meta');
+	metaCode.setAttribute('name', 'viewport');
+	metaCode.setAttribute('content', 'width=device-width,initial-scale=1.0');
+	document.head.appendChild(metaCode);
+
+})();
